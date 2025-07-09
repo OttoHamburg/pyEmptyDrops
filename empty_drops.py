3a2c5f483fc097cc3fc264243eb431da9bf256b8
Oskar Haupt vsc <otto_hamburg@gmx.de>
2025-07-09 22:51:00 +0200

    Add multinomial probability calculations

--- empty_drops.py
+++ empty_drops.py
@@ -1,12 +1,13 @@
 import scanpy as sc
 import numpy as np
+from fontTools.subset.svg import update_glyph_href_links
+from numba import typeof
 from scipy.sparse import csr_matrix
+from scipy.optimize import minimize, minimize_scalar
+from scipy.special import gamma, gammaln, factorial  # gamma function and Log of the gamma function
 import scipy.stats as ss
 from collections import Counter
-
-
-
-
+from tqdm import tqdm
 
 
 """
@@ -14,25 +15,24 @@ Functionality:  Checks a CSR matrix for float values.
                 Prompts the user to continue or exit if floats are found.
 Returns:        True/False.
 """
-def check_count_matrix_values(m):
+def check_matrix_contain_floats(matrix):
     try:
-        check_values(m)
+        check_values(matrix)
     except ValueError as e:
         # Catch the specific ValueError raised
         print(e)
-        response = input("Do you want to continue processing? (yes/no) otherwise type (int) if you want to convert all floats to integers: ")
+        #response = input("Do you want to continue processing? (yes/no) otherwise type (int) if you want to convert all floats to integers: ")
+        response = 'int'
         if response.lower() == 'yes':
             print("Continuing with processing...")
-            return True, m
+            return True, matrix
         elif response.lower() == 'int':
-            m = m.astype(int)  # Convert all float values to integers
+            matrix = matrix.astype(int)  # Convert all float values to integers
             print("Converted matrix to integers.")
-            return True, m  # Return the converted matrix
+            return True, matrix  # Return the converted matrix
         else:
             print("Exiting process.")
             return False, None
-
-
 
 """
 Functionality:  Counts float values in the count_matrix and raises a ValueError
@@ -54,11 +54,144 @@ def check_values(matrix):
         print("The provided matrix is not a CSR matrix.")
 
 
+def good_turing_ambient_pool(data, low_count_gene_sums):
+    # flatten the 1x22040 2D np-matrix since it led to errors in hashing using "Counter"
+    counts = np.asarray(low_count_gene_sums)
+    counts = counts.flatten() if counts.ndim > 1 else counts
+
+    # Calculate frequency distribution (sorted increasing)
+    frequency_distribution = Counter(counts)
+    # Total number of unique items
+    n = sum(frequency_distribution.values())
+    # Initialize Good-Turing proportions dictionary
+    good_turing_proportions = {}
+
+    # Calculate Good-Turing proportions to avoid having genes with zero counts in the ambient pool
+    for r in tqdm(range(max(counts) + 1), desc="Calculating Good-Turing Proportions"):
+        n_r = frequency_distribution.get(r, 0)  # Number of items with count r
+        n_r_plus_1 = frequency_distribution.get(r + 1, 1)  # Number of items with count r + 1
+
+        if n_r > 0:  # Only calculate for observed frequencies
+            # habe jetzt einfach n_r_plus_1 auf =1 gesetzt falls es nicht ex., sonst vielleicht auch einfach and n_r_plus_1 > 0 fordern?
+            good_turing_proportions[r] = (r + 1) * n_r_plus_1 / n
+
+    # Handle unseen events (count = 0) => common practice = N1/N
+    good_turing_proportions[0] = frequency_distribution.get(1, 0) / n #ändert in dem fall nix, da erstes N0 = 0, somit (0+1)*N1 und somit N1/N gilt
+
+    # Normalize the proportions to sum to 1
+    total_proportion = sum(good_turing_proportions.values())
+    for r in tqdm(good_turing_proportions, desc="Normalizing Proportions"):
+        good_turing_proportions[r] /= total_proportion
+
+    # Ensure normalization is correct
+    normalized_total = sum(good_turing_proportions.values())
+    assert abs(normalized_total - 1.0) < 1e-6, f"Proportions do not sum to 1: {normalized_total}"
+
+    # Create an array to hold results
+    gene_expectations = np.zeros_like(counts, dtype=float)
+    # Fill in the results regarding the proportions
+    ambient_proportions = {}
+
+    # Get all gene_names
+    gene_names = data.var_names
+
+    for i, count in tqdm(enumerate(counts), desc="Filling Ambient Proportions", total=len(counts)):
+        proportion = good_turing_proportions.get(count, 0)
+        gene_expectations[i] = proportion
+        ambient_proportions[gene_names[i]] = proportion
+
+    return gene_names, ambient_proportions, gene_expectations
+
+
+def estimate_alpha(ambient_set_g, gene_proportions):
+    """
+    Estimates alpha by maximizing the Dirichlet-Multinomial likelihood.
+
+    # we need to compute the log-likelihood for all barcodes in the ambient set G, by summing the log L_b values for each barcode
+
+    Parameters:
+        counts (numpy.ndarray): Gene-by-barcode count matrix (G x B).
+        proportions (numpy.ndarray): Ambient RNA proportions (G,).
+
+    Returns:
+        float: Estimated alpha.
+    """
+    alpha = 1.0 # initial guessed alpha
+    total_log_likelihood = 0
+    product = [1.0]
+
+    batch_size = ambient_set_g.shape[0] // 100
+
+    # iterate over all barcodes
+    for i in tqdm(range(0, ambient_set_g.shape[0] - 1, batch_size), desc="Estimate (global) alpha", position=0, leave=True):
+        # batch extraction
+        batch = ambient_set_g[i: i+ batch_size]
+
+        # p_g = proportions
+        total_counts = batch.X.sum(axis=0).A1
+        # reduce dimension of total_counts
+
+        y_gb = np.asarray(batch.X.A)  # .flatten() #convert sparse matrix to dense array for calculations
+        #alpha_p_g = gene_proportions * alpha
+        # product = product * np.log( gamma(y_gb + alpha_p_g) / ( factorial(y_gb) * gamma(alpha_p_g) ) )
+
+        product = product * np.log((gamma(y_gb + gene_proportions)) / (factorial(y_gb) * (gene_proportions)))
+    l_b = (factorial(total_counts) * gamma(alpha)) / np.log(gamma(total_counts + alpha) * product)
+
+    # Convert L_b to log-space and sum
+    l_b = np.nanmean(l_b)
+    total_log_likelihood += np.log(l_b)
+    print(f"total_log_likelihood: {total_log_likelihood}")
+
+    return -total_log_likelihood # Return negative for minimization
+
+
+
+def maximum_likelihood_estimate(alpha, batch_barcodes, p_g, batch_size=10000):
+    # p_g = proportions
+    total_counts = batch_barcodes.X.sum(axis=0).A1
+    # reduce dimension of total_counts
+    product = 1.0
+
+    y_gb = np.asarray(batch_barcodes.X.A)#.flatten() #convert sparse matrix to dense array for calculations
+    alpha_p_g = p_g * alpha
+    #product = product * np.log( gamma(y_gb + alpha_p_g) / ( factorial(y_gb) * gamma(alpha_p_g) ) )
+    product = product * ( np.log(gamma(y_gb + alpha_p_g)) / (factorial(y_gb) * (alpha_p_g)) )
+    l_b = ( factorial(total_counts) * gammaln(alpha) ) / np.log( gamma(total_counts + alpha) * product )
+
+    return l_b
+
+    # total_counts = data.X[barcode].sum()
+    # product = 1
+    # rows, cols = data.X[barcode].nonzero()
+    #
+    # for gene_idx in cols:
+    #     y_gb = data.X[barcode, gene_idx]
+    #
+    #     gene_name = data.var_names[gene_idx]
+    #     p_g = ambient_proportions[gene_name]
+    #
+    #     alpha_p_g = alpha * p_g
+    #
+    #     if y_gb== 0:
+    #         print("y_gb: ", y_gb)
+    #     if p_g == 0:
+    #         print("p_g: ", p_g)
+    #
+    #     # log reingefuchst, da sonst das produkt der MLE immer kleiner wird und so gegen 0 geht
+    #     product = product * log( gamma(y_gb + alpha_p_g) / ( factorial(y_gb) * gamma(alpha_p_g) )
+    #                         )
+    #
+    # l_b = ( factorial(total_counts) * gamma(alpha) ) / ( gamma(total_counts + alpha) * product )
+    #
+    # return l_b
+
+
 
 """
 Distinguish between droplets containing cells and ambient RNA in a droplet-based single-cell RNA sequencing experiment.
 ------ INPUT ------
-m             = A numeric matrix object - usually a dgTMatrix or dgCMatrix - containing droplet data prior to
+data.X             = A numeric matrix object - usually a dgTMatrix or dgCMatrix - containing droplet data prior to
                 any filtering or cell calling. Columns represent barcoded droplets, rows represent genes.
 lower         = numeric scalar specifying the lower bound on the total UMI count
 retain        = Barcodes that contain more than retain total counts are always retained. This ensures that large
@@ -80,156 +213,53 @@ The metadata of the output DataFrame will contains the ambient profile in ambien
 of alpha, the specified value of lower (possibly altered by use.rank) and the number of iterations in niters.
 For emptyDrops, the metadata will also contain the retention threshold in retain.
 """
-def empty_drops(m, lower=100, retain=None, barcode_args: list = None, test_ambient=False):
+def empty_drops(data, lower=100, retain=None, barcode_args: list = None, test_ambient=False):
+    # remove all genes which aren't representif in one barcode
+    print(f"{(data.X.sum(axis=0).A1 == 0).sum()} genes filtered out since sum(counts) over the gene was 0.")
+    sc.pp.filter_genes(data, min_counts=1)
 
-    stats = test_empty_drops(m, lower=lower, test_ambient=test_ambient)
+    low_count_barcode_mask = data.X.sum(axis=1).A1 <= 50  # axis=1 summing every value in row
+    high_count_barcodes = data[low_count_barcode_mask==False]
+    # Sum counts for each gene (axis=0/column) but only for low-count barcodes (-> in low_count_mask rows labeled as True)
+    low_count_gene_sums = data.X[low_count_barcode_mask].sum(axis=0)
+
+    print
+
+
+    # ----------------------
+    # CREATE AMBIENT PROFILE
+    # ----------------------
+    # gene_expectations is np array with 20025 genes, ambient_proportions is dict with genes and their expectation
+    gene_names, ambient_proportions, gene_expectations = good_turing_ambient_pool(data, low_count_gene_sums)
+
+    # calculate global alpha out of low_count_barcodes
+    ambient_set_g = data[low_count_barcode_mask]
+    alpha = estimate_alpha(ambient_set_g, gene_expectations)
+    # dauert knapp 47 Stunden
+
+
+    # calculate likelihoods using global alpha for high_count_barcodes
+    likelihoods = []
+    for idx, barcode in enumerate (high_count_barcodes):
+        likelihood = maximum_likelihood_est
+
+    # test for barcode 2
+    likelihood_barcode = maximum_likelihood_estimate(1, 2, ambient_proportions, data)
+    print("likelihood_barcode 2: ", likelihood_barcode)
 
 
 
 
+    # log_probs = np.array([ss.multinomial.logpmf(data.X[:, i], n=data.X[:, i].sum(), p=0.1) for i in range(data.X.shape[1])])
 
-""" 
-obtains an estimate of the composition of the ambient pool of RNA based on the barcodes with total UMI counts less than or equal to lower
-"""
-def test_empty_drops(m, lower=100, niters = 10000, test_ambient = False, ignore = None, alpha = None, round = True, by_rank = None):
-    # obtains an estimate of the composition of the ambient pool of RNA based on the barcodes with total UMI counts less than or equal to lower
-    # Counts for the low-count barcodes are pooled together, and an estimate of the proportion vector for the ambient pool is calculated using goodTuringProportions
-    ambient_proportions = estimateAmbience(m, lower)
-
-    # Test for significant deviation from ambient proportions for each barcode above lower
-    high_count_mask = m.sum(axis=1).A1 > lower  # Mask for high-count barcodes
-    high_count_barcodes = m[high_count_mask]
-
-    low_count_mask = m.sum(axis=1).A1 <= lower  # Mask for high-count barcodes
-    low_count_barcodes = m[low_count_mask]
-    low_count_mean = m[low_count_mask].mean(axis=0)
-    low_count_prop = low_count_mean / low_count_mean.sum()
-
-    significant_deviations = {}
-
-    for i in range(high_count_barcodes.shape[0]):
-        barcode_counts = high_count_barcodes[i].A  # Get counts for the current barcode
-        expected_counts = ambient_proportions * barcode_counts.sum()
-        deviation = np.abs(barcode_counts - expected_counts)
-
-        if np.any(deviation > expected_counts * 0.1):  # check if 10% deviation is significant
-            significant_deviations[i] = deviation
+    # significant_deviations = compute_significant_deviations(high_count_barcodes, ambient_proportions, gene_names)
+    #print(f"Identified {len(significant_deviations)} barcodes with significant deviations.")
 
     # wir haben nun high_count barcodes gegen deviation getestet, teste NUN *ALLE BARCODES* gegen deviations
     # the null model assumes that the counts in each barcode (bzw. droplet) originate solely from the ambient RNA pool,
-    # meaning they are fully random and representative of the background noise.
+    # meaning they are FULLY random (+++representative of the background noise).
 
-    # For each barcode, the probability of obtaining its count vector based on the null model is computed.
-    log_probs = np.array([ss.multinomial.logpmf(m[:, i].A1, n=m[:, i].sum(), p=prop)
-                          for i in range(m.shape[1])])
-
-    # Then, niters count vectors
-    # are simulated from the null model. The proportion of simulated vectors with probabilities lower than the observed multinomial
-    # probability for that barcode is used to calculate the p-value.
-
-    # We use this Monte Carlo approach as an exact multinomial p-value is difficult to calculate. However, the p-value is lower-bounded
-    # by the value of niters (Phipson and Smyth, 2010), which can result in loss of power if niters is too small. Users can check whether
-    # this loss of power has any practical consequence by checking the Limited field in the output. If any barcodes have Limited=TRUE but
-    # does not reject the null hypothesis, it suggests that niters should be increased.
-
-    # The stability of the Monte Carlo $p$-values depends on niters, which is only set to a default of 10000 for speed. Larger values
-    # improve stability with the only cost being that of time, so users should set niters to the largest value they are willing to wait for.
-
-    # The ignore argument can also be set to ignore barcodes with total counts less than or equal to ignore. This differs from the lower
-    # argument in that the ignored barcodes are not necessarily used to compute the ambient profile. Users can interpret ignore as the
-    # minimum total count required for a barcode to be considered as a potential cell. In contrast, lower is the maximum total count below
-    # which all barcodes are assumed to be empty droplets.
-
-    # Implement Monte Carlo approach to compute p-values for each barcode based on the multinomial sampling
-    p_values = {}
-
-    # Generate niters simulated count vectors from the ambient proportions
-    monte_carlo_counts = np.random.multinomial(m.sum(), ambient_proportions, size=niters)
-
-    for idx, deviation in significant_deviations.items():
-        barcode_counts = high_count_barcodes[idx].A.squeeze()
-        # Observing count vector in the multinomial distribution
-        obs_ll = ss.multinomial.pmf(barcode_counts, n=barcode_counts.sum(), p=ambient_proportions)
-
-        simulated_ll = np.array([
-            ss.multinomial.pmf(sim_counts, n=sim_counts.sum(), p=ambient_proportions)
-            for sim_counts in monte_carlo_counts
-        ])
-
-        # Calculate p-value by comparing observed log-likelihood to the simulated log-likelihoods
-        p_value = np.sum(simulated_ll < obs_ll) / niters
-        p_values[idx] = p_value
-
-
-    return significant_deviations
-
-
-
-""" 
-Estimate the composition of the ambient pool of RNA based on barcodes with total UMI counts less than or equal to 'lower'.
-    Parameters:
-        m (csr_matrix): A CSR matrix containing RNA count data.
-        lower (float): The threshold for low UMI counts.
-    Returns:
-        np.ndarray: Estimated proportions for each transcript in the ambient pool. 
-"""
-def estimateAmbience(m, lower, by_rank = None, good_turing = True):
-    # creates mask for low-count barcodes (finds every cell with less than lower active genes)
-    low_count_mask = m.sum(axis=1).A1 <= lower #axis=1 summing every value in row
-    # Sum counts for each gene (axis=0/column) but only for low-count barcodes (-> in low_count_mask rows labeled as True)
-    low_count_sums = m[low_count_mask].sum(axis=0)
-    # Estimate proportions using Good-Turing
-    proportions = goodTuringProportions(low_count_sums)
-    return proportions
-
-
-""" 
-Estimate Good-Turing proportions based on counts.
-    Parameters:
-        counts (np.ndarray): A 1D array of counts for each gene (but only for barcodes with total <lower)
-    Returns:
-        np.ndarray: Estimated proportions for each gene. 
-"""
-def goodTuringProportions(counts):
-    # flatten the 1x22040 2D np-matrix since it let to errors in hasing using "Counter"
-    if isinstance(counts, np.matrix):
-        counts = np.asarray(counts)
-
-    counts = counts.flatten() if counts.ndim > 1 else counts
-
-    total_counts = counts.sum()
-    # Handle case where total_counts is zero
-    if total_counts == 0:
-        # create array full of zeros shaped like counts/low_count_sums array
-        return np.zeros_like(counts)
-    # Calculate frequency distribution (sorted increasing)
-    frequency_distribution = Counter(counts)
-    # Total number of unique items
-    n = sum(frequency_distribution.values())
-    # Initialize Good-Turing proportions dictionary
-    good_turing_proportions = {}
-
-    # Calculate Good-Turing proportions
-    for r in range(max(counts) + 1):
-        n_r = frequency_distribution.get(r, 0)  # Number of items with count r
-        n_r_plus_1 = frequency_distribution.get(r + 1, 0)  # Number of items with count r + 1
-
-        if n_r > 0:  # Only calculate for observed frequencies
-            good_turing_proportions[r] = (r + 1) * n_r_plus_1 / n
-
-    # Handle unseen events (count = 0) => common praxis = N1/N
-    good_turing_proportions[0] = frequency_distribution.get(1, 0) / n
-
-    # rreate an array for hold results
-    results = np.zeros_like(counts)
-
-    # fill in the results regarding the proportions
-    for r in range(len(results)):
-        if r in good_turing_proportions:
-            results[r] = good_turing_proportions[r]
-
-    return results
-
+    # For every barcode, the probability of obbtaining the specific count vector based on the null model is computed.
 
 
 def main():
@@ -239,21 +269,17 @@ def main():
     filtered_adata = sc.read_10x_h5(filtered_h5_path, gex_only=True)
     print("LOADED MATRICES")
 
-    go_on, m = check_count_matrix_values(raw_adata.X)
+    go_on, m = check_matrix_contain_floats(raw_adata.X)
+
+    # assign the new count matrix with floats changed to ints (if user wanted)
+    raw_adata.X = m
+
     if not go_on:
         print("EXIT PROGRAM")
         return 0
     else:
         print("START EMPTY_DROPS")
-        empty_drops(m) #only pass the counts to the function
+        empty_drops(raw_adata)
 
 
 main()
-
-
-
-
-
-
-
-
